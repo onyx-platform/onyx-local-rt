906f7fa739417c9ecaef2f5de0d5a081f2b1caa4
Michael Drogalis <mjd3089@rit.edu>
2016-12-05 10:31:32 -0800

    Bump version.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.onyxplatform/onyx-local-rt "0.9.11.2"
+(defproject org.onyxplatform/onyx-local-rt "0.9.15.0"
   :description "A local, pure, deterministic runtime for Onyx"
   :url "https://github.com/onyx-platform/onyx-local-rt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.onyxplatform/onyx "0.9.15-20161129.054922-5"]
-                 [clojure-future-spec "1.9.0-alpha13"]
+                 [clojure-future-spec "1.9.0-alpha14"]
                  [com.stuartsierra/dependency "0.2.0"]]
   :plugins [[codox "0.8.8"]])
