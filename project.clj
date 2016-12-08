04b6632254096c126507f0fc9f4cb0c0c050b4e7
Michael Drogalis <mjd3089@rit.edu>
2016-12-08 11:22:28 -0800

    Bump Onyx version.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.onyxplatform/onyx "0.9.16-20161208.183703-3"]
+                 [org.onyxplatform/onyx "0.9.16-20161208.192149-6"]
                  [clojure-future-spec "1.9.0-alpha14"]
                  [com.stuartsierra/dependency "0.2.0"]]
   :plugins [[codox "0.8.8"]])
