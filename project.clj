ad87785991ccc68ed404814d9f965ef85c8e203f
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-02-01 15:14:16 -0800

    Add clojars releases to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "https://github.com/onyx-platform/onyx-local-rt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env
+                              :password :env
+                              :sign-releases false}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env
+                             :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.onyxplatform/onyx "0.10.0-alpha1"]
                  [clojure-future-spec "1.9.0-alpha14"]
