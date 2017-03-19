5e8436d7ca6ac21d3d9a8b0c02043f6d0df7166e
Michael Drogalis <mjd3089@rit.edu>
2017-03-19 13:31:29 -0700

    0.10.0.0-beta8.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-local-rt "0.10.0.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-local-rt "0.10.0.0-beta8"
   :description "A local, pure, deterministic runtime for Onyx"
   :url "https://github.com/onyx-platform/onyx-local-rt"
   :license {:name "Eclipse Public License"
@@ -12,7 +12,7 @@
                              :password :env
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.onyxplatform/onyx "0.10.0-alpha1"]
+                 [org.onyxplatform/onyx "0.10.0-beta8"]
                  [clojure-future-spec "1.9.0-alpha14"]
                  [com.stuartsierra/dependency "0.2.0"]]
   :plugins [[codox "0.8.8"]])
