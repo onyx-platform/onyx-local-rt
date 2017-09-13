fa0fa00193404d301c8bc8ec76f53eb676a75d01
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-09-13 14:39:36 -0700

    Switch back to non-voom onyx.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.0-20170913_201530-gd7db048"]
+                 [org.onyxplatform/onyx "0.11.0-alpha3"]
                  [org.onyxplatform/onyx-spec "0.11.0.0-alpha3"]
                  [com.stuartsierra/dependency "0.2.0"]]
   :plugins [[codox "0.8.8"]])
