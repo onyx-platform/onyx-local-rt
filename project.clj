4e5fe23f82ee0f24a91caeb0897be517181917e0
OnyxBot <michael.drogalis@onyxplatform.org>
2017-10-16 05:42:58 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-local-rt "0.11.1.0-alpha1"
+(defproject org.onyxplatform/onyx-local-rt "0.11.1.0-SNAPSHOT"
   :description "A local, pure, deterministic runtime for Onyx"
   :url "https://github.com/onyx-platform/onyx-local-rt"
   :license {:name "Eclipse Public License"
