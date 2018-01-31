f1128a2eff8a8c8296c7ec3c2bc3283dd013db46
OnyxBot <michael.drogalis@onyxplatform.org>
2018-01-31 18:28:47 +0000

    Release version 0.12.6.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-local-rt "0.12.5.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-local-rt "0.12.6.0"
   :description "A local, pure, deterministic runtime for Onyx"
   :url "https://github.com/onyx-platform/onyx-local-rt"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.12.5"]
+                 [org.onyxplatform/onyx "0.12.6"]
                  [org.onyxplatform/onyx-spec "0.11.0.0-alpha3"]
                  [com.stuartsierra/dependency "0.2.0"]]
   :plugins [[codox "0.8.8"]
